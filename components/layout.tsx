2e4d0e4d9e4bcd0341f53e07c08078cbd4e1fe68
BoYeonJang <uko010199@gmail.com>
2022-10-28 22:05:31 +0900

    layout에 헤더 추가

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,11 +1,17 @@
+import Head from "next/head";
 import Header from "./header";
 import Footer from "./footer";
 
 export default function Layout({ children }: any) {
   return (
     <div>
+      <Head>
+        <title>포켓몬 전부 말할 수 있을까? 게임</title>
+        <meta name="description" content="포켓몬 전부 말할 수 있을까? 게임" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Header />
-      <h1>레이아웃</h1>
+      {/* <h1>레이아웃</h1> */}
       <div>{children}</div>
       <Footer />
     </div>
