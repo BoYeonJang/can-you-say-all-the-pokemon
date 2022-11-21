78ee080cf6cb79a94dc272e2452bf99ae404a976
BoYeonJang <uko010199@gmail.com>
2022-11-21 19:48:29 +0900

    타이머 및 랭킹페이지 임시 추가

---
+++ pages/ranking.tsx
@@ -0,0 +1,12 @@
+import Layout from "../components/layout";
+import styles from "../styles/Home.module.css";
+
+export default function Ranking() {
+  return (
+    <Layout>
+      <main className={styles.main}>
+        <h1 className={styles.title}>클리어 타임 랭킹</h1>
+      </main>
+    </Layout>
+  );
+}
